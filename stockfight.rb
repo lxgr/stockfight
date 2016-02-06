1950c28a5d0abd065455d11be7e9829cb922fdae
Lukas Ribisch <lukas.ribisch@gmail.com>
2016-02-06 13:17:01 +0100

    refactoring

--- stockfight.rb
+++ stockfight.rb
@@ -5,9 +5,9 @@ require 'httparty'
 require 'json'
 require_relative 'sfconfig'
 
-$account = "YAN34122142"
-$venue = "GFDEX"
-$stock = "HNH"
+$account = "IOS64914507"
+$venue = "EPEX"
+$stock = "LTCM"
 
 class TxList
     def initialize
@@ -241,53 +241,64 @@ end
 $db = TxList.new
 
 
-if __FILE__ == $0
-# Trivial market-making bot
+class MarketMaker
+    
+    def initialize(apikey, account, venue, stock, maxinv)
+        @client = StockClient.new($apikey, $account, $venue, $stock)
+        @inv = 0
+        @maxinv = maxinv
+    end
 
-    client = StockClient.new($apikey, $account, $venue, $stock)
-    inv = 0
-    maxinv = 300
-    last = client.quote.last
-
-    while true do
-        open = TxList.new
-        spread = (last/100)-4
-        bid = last-(spread/2)
-        ask = last+(spread/2)
-        nask = [1,maxinv + inv].max
-        nbid = [1,maxinv - inv].max
-        puts "Bid #{nbid}@#{bid}, ask #{nask}@#{ask} – @spread #{ask-bid}"
-        if (nbid > 0)
-            open.insert(Order.new(client, nbid, bid, "buy", "limit"))
-        end
-        if (nask > 0)
-            open.insert(Order.new(client, nask, ask, "sell", "limit"))
-        end
-        open.post_all!
+    def makemarket!
+        last = @client.quote.last
 
         while true do
-            # As we always have at least one order for each side on the book,
-            # we are guaranteed to find out about any price moves.
-            # (But not about market makers with a narrower spread...)
-            sleep 1
-            open.update_all!
-            if (open.total_fills > 0)
+            open = TxList.new
+            spread = (last/100)-4
+            bid = last-(spread/2)
+            ask = last+(spread/2)
+            nask = [1,@maxinv + @inv].max
+            nbid = [1,@maxinv - @inv].max
+            puts "Bid #{nbid}@#{bid}, ask #{nask}@#{ask} – @spread #{ask-bid}"
+            if (nbid > 0)
+                open.insert(Order.new(@client, nbid, bid, "buy", "limit"))
+            end
+            if (nask > 0)
+                open.insert(Order.new(@client, nask, ask, "sell", "limit"))
+            end
+            open.post_all!
+
+            while true do
+                # As we always have at least one order for each side on the book,
+                # we are guaranteed to find out about any price moves.
+                # (But not about market makers with a narrower spread...)
+                sleep 1
+                open.update_all!
+                if (open.total_fills > 0)
+                    break
+                end
+            end
+
+            open.cancel_all!
+            @inv += open.pos_shares
+
+            # The new mid price is the weighted average of all executed
+            # transactions for the outstanding orders.
+            last = open.avg_fill
+
+            # Failsafe. This should only be reached after extreme
+            # price swings or implementation bugs...
+            puts "Current inv: #{@inv}"
+            if (@inv.abs > 2*@maxinv)
+                puts "That's too much, please fix it!"
                 break
             end
         end
-
-        open.cancel_all!
-        inv += open.pos_shares
-
-        # The new mid price is the weighted average of all executed
-        # transactions for the outstanding orders.
-        last = open.avg_fill
-
-        puts "Current inv: #{inv}"
-        if inv.abs > 2*maxinv
-            puts "That's too much, please fix it!"
-            break
-        end
     end
 
+end
+        
+
+if __FILE__ == $0
+    MarketMaker.new($apikey, $venue, $account, $stock, 300).makemarket!
 end
