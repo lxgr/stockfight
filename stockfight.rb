0368143ad41ef81210a11db570fd2b8182d6226d
Lukas Ribisch <lukas.ribisch@gmail.com>
2016-02-06 13:09:51 +0100

    refactoring

--- stockfight.rb
+++ stockfight.rb
@@ -135,13 +135,13 @@ class StockClient
 
     def update_order(order)
         response = HTTParty.get("#{Base_url}/venues/#{@venue}/stocks/#{@stock}/orders/#{order.id}", :headers => {"X-Starfighter-Authorization" => @apikey})
-        raise "Error updating!" unless response.code.between?(200,300)
+        raise "Error updating!" unless http_ok?(response.code)
         handle_result(order, response)
     end 
 
     def cancel_order(order)
         response = HTTParty.delete("#{Base_url}/venues/#{@venue}/stocks/#{@stock}/orders/#{order.id}", :headers => {"X-Starfighter-Authorization" => $apikey})
-        raise "Error deleting!" unless response.code.between?(200,300)
+        raise "Error deleting!" unless http_ok?(response.code)
         handle_result(order, response)
     end
         
@@ -168,6 +168,10 @@ class StockClient
           "orderType" => order.type
         }
         return JSON.generate(json)
+    end
+
+    def http_ok?(code)
+        return code.between?(200,300)
     end
 end
 
